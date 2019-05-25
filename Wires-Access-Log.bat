276085c0573ac37eb186a7e8cc1081ed05e8af3a
Mathias Weyland <lt-betrieb@hb9uf.ch>
2019-05-25 12:54:25 +0200

    Adding windows batch file for autostart -- thanks to Peter HB9DWW

---
+++ Wires-Access-Log.bat
@@ -0,0 +1,3 @@
+cd %HOMEPATH%\Documents\unconfusion
+start python logger.py %HOMEPATH%\Documents\WIRESXA\AccHistory\WiresAccess.log
+start python web.py %HOMEPATH%\Documents\WIRESXA\AccHistory\WiresAccess.log
